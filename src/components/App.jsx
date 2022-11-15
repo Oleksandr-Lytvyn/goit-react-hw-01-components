ae3c0a9880d76f3c66230606beda7582094a8abc
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-11-15 20:28:59 +0200

    task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import user from '../JSON/user';
-console.log(user);
+import data from '../JSON/data';
+console.log(data);
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      ava={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        ava={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} title={null} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+export function Statistics(props) {
+  console.log(props.stats);
+  return (
+    <section className="statistics">
+      {props.title && <h2 className="title">{props.title}</h2>}
+
+      <ul className="stat-list">
+        {props.stats.map(item => (
+          <li className="item" key={item.id}>
+            <span className="label">{item.label}</span>
+            <span className="percentage">{item.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}

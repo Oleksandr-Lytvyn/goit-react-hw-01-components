be5a70249ce02dc5f7f144301d3823f35f2dbb38
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-11-23 21:33:18 +0200

    add propTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import PropTypes from 'prop-types';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../JSON/user';
 import data from '../JSON/data';
@@ -23,4 +24,22 @@ export const App = () => {
       <TransactionHistory tansactions={transactions} />
     </>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  ava: PropTypes.string,
+  stats: PropTypes.object,
+};
+Statistics.propTypes = {
+  stats: PropTypes.array,
+  title: PropTypes.string,
+};
+FriendList.propTypes = {
+  friends: PropTypes.array,
+};
+TransactionHistory.propTypes = {
+  tansactions: PropTypes.array,
 };

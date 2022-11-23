1e437252f3ae1d491eb5eab81f9a9036fa2bb3f9
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-11-23 21:11:43 +0200

    add keys

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,37 +3,32 @@ import { RiFlagFill } from 'react-icons/ri';
 export function FriendList({ friends }) {
   return (
     <ul className="friend-list">
-      {friends.map(item => (
-        <FriendListItem
-          id={item.id}
-          avatar={item.avatar}
-          name={item.name}
-          isOnline={item.isOnline}
-        />
-      ))}
+      <FriendListItem friends={friends} />
     </ul>
   );
 }
 
-function FriendListItem({ avatar, name, isOnline, id }) {
-  return (
-    <li
-      key={id}
-      className="item"
-      style={{ backgroundColor: isOnline ? '	#F0FFF0' : '	#FFF5EE' }}
-    >
-      {isOnline ? (
-        <span className="status" style={{ color: 'green' }}>
-          <RiFlagFill />
-        </span>
-      ) : (
-        <span className="status" style={{ color: 'red' }}>
-          <RiFlagFill />
-        </span>
-      )}
+function FriendListItem({ friends }) {
+  return friends.map(({ avatar, name, isOnline, id }) => {
+    return (
+      <li
+        key={id}
+        className="item"
+        style={{ backgroundColor: isOnline ? '	#F0FFF0' : '	#FFF5EE' }}
+      >
+        {isOnline ? (
+          <span className="status" style={{ color: 'green' }}>
+            <RiFlagFill />
+          </span>
+        ) : (
+          <span className="status" style={{ color: 'red' }}>
+            <RiFlagFill />
+          </span>
+        )}
 
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
-    </li>
-  );
+        <img className="avatar" src={avatar} alt="User avatar" width="48" />
+        <p className="name">{name}</p>
+      </li>
+    );
+  });
 }

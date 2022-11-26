81e5c37c25d7f9bbef4eb00de09c49a32a395ac4
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-11-26 16:40:02 +0200

    delete console log

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,6 +1,5 @@
 import css from '../TransactionHistory/transactionHistory.module.css';
 
-console.log(css);
 export function TransactionHistory({ tansactions }) {
   return (
     <table className={css.transaction_history}>
